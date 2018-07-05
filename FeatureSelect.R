433c936d227c8ea002241cce927c4a1406ca3418
Nirmalya Bandyopadhyay <nirmalya@broadinstitute.org>
2018-07-05 17:26:59 -0400

    Updated related to some logging

--- FeatureSelect.R
+++ FeatureSelect.R
@@ -4,11 +4,12 @@ library(DecisionAnalysis)
 
 'Selection of features (probes) from NanoString data
 
-Usage: FeatureSelect.R -i <infile> -o <outdir> [--feature_count <feature_count> --use_all --no_probe_anno --sep <sep>]
+Usage: FeatureSelect.R -i <infile> -o <outdir> -p <prefix> [--feature_count <feature_count> --use_all --no_probe_anno --sep <sep>]
 
 options:
   -i <infile> --infile <infile>
   -o <outdir> --outdir <outdir>
+  -p <prefix> --prefix <prefix>
   --feature_count <feature_count> [default: 10]
   --sep <sep> [default: comma]
   --use_all
@@ -19,6 +20,7 @@ print(opts)
 
 infile <- opts$infile
 outdir <- opts$outdir
+prefix <- opts$prefix
 feature_count <- as.numeric(opts$feature_count)
 sep <- opts$sep
 use_all <- opts$use_all
@@ -26,6 +28,7 @@ no_probe_anno <- opts$no_probe_anno
 
 print(paste0("infile: ", infile))
 print(paste0("outdir: ", outdir))
+print(paste0("prefix: ", prefix))
 print(paste0("feature_count: ", feature_count))
 print(paste0("sep: ", sep))
 print(paste0("use_all: ", use_all))
@@ -38,4 +41,11 @@ if (sep == "comma") {
     lsep = "\t"
 } 
 
-featureSelect(infile = infile, outdir = outdir, feature_count = feature_count, use_all = use_all, no_probe_anno = no_probe_anno, sep = lsep)
+dir.create(outdir)
+logfile <- paste0(outdir, "/", prefix , "_log.txt")
+sink(logfile)
+
+
+featureSelect(infile = infile, outdir = outdir, prefix = prefix, feature_count = feature_count, use_all = use_all, no_probe_anno = no_probe_anno, sep = lsep)
+
+sink()
--- Validate.R
+++ Validate.R
@@ -3,12 +3,13 @@ library(DecisionAnalysis)
 
 'Validation of features (probes) on new Nanostring data
 
-Usage: Validate.R -t <train_file> -v <validate_file> -o <outdir> [--feature_count <feature_count> --sep <sep>]
+Usage: Validate.R -t <train_file> -v <validate_file> -o <outdir> -p <prefix> [--feature_count <feature_count> --sep <sep>]
 
 options:
   -t <train_file> --train_file <train_file>
   -v <validate_file> --validate_file <validate_file>
   -o <outdir> --outdir <outdir>
+  -p <prefix> --prefix <prefix>
   --feature_count <feature_count> [default: -1]
   --sep <sep> [default: comma]' -> doc
 
@@ -18,12 +19,14 @@ print(opts)
 train_file <- opts$train_file
 validate_file <- opts$validate_file
 outdir <- opts$outdir
+prefix <- opts$prefix
 sep <- opts$sep
 feature_count <- as.numeric(opts$feature_count)
 
 print(paste0("train_file: ", train_file))
 print(paste0("validate_file: ", validate_file))
 print(paste0("outdir: ", outdir))
+print(paste0("prefix: ", prefix))
 print(paste0("feature_count: ", feature_count))
 print(paste0("sep: ", sep))
 
@@ -34,5 +37,8 @@ if (sep == "comma") {
     lsep = "\t"
 }
 
-validate(train_file = train_file, validate_file = validate_file, outdir = outdir, feature_count = feature_count, sep = lsep)
-
+dir.create(outdir)
+logfile <- paste0(outdir, "/", prefix , "_log.txt")
+sink(logfile)
+validate(train_file = train_file, validate_file = validate_file, outdir = outdir, prefix = prefix, feature_count = feature_count, sep = lsep)
+sink()
